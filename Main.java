6c99da6461a2d3c6c26639ac6a488a2f59af7e7c
Anderson Aguiar de Souza <31575796+AndersonS001@users.noreply.github.com>
2017-09-02 18:40:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import javax.swing.JOptionPane;
 /**
  * 
- * @author ander
+ * @author Anderson Aguiar de Souza 22216001-2
  *Classe principal da função
  */
 public class Main {
@@ -13,18 +13,19 @@ public class Main {
 		//CriaVeiculo v2 = new CriaVeiculo();
 		/**Declarando um objeto usuário*/
 		Usuario u2 = new Usuario(JOptionPane.showInputDialog("Por favor, digite seu nome: "), 
-				Integer.parseInt(JOptionPane.showInputDialog("Por favor, digite sua idade: ")));
-		
-		/**Fazendo testes dos métodos*/
-		u2.ligarCarro();
-		u2.virarDireita();
-		u2.ligaAr();
-		u2.ligarCarro();
-		u2.ligaAr();
-		u2.fecharJanela();
-		u2.ligaAr();
-		u2.travaPorta();
-		u2.desligarCarro();
+				JOptionPane.showConfirmDialog(null,"Você possui mais de 18 anos? ",null, JOptionPane.YES_NO_OPTION));
 
+		if(u2.getUser()) {
+			/**Fazendo testes dos métodos*/
+			u2.ligarCarro();
+			u2.virarDireita();
+			u2.ligaAr();
+			u2.ligarCarro();
+			u2.ligaAr();
+			u2.fecharJanela();
+			u2.ligaAr();
+			u2.travaPorta();
+			u2.desligarCarro();
+		}
 	}
 }
