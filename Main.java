51f77c17920eef74e72a4eae89b0ce822d12d9aa
Anderson Aguiar de Souza <31575796+AndersonS001@users.noreply.github.com>
2017-09-02 18:21:38 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,21 @@
 import javax.swing.JOptionPane;
-
+/**
+ * 
+ * @author ander
+ *Classe principal da função
+ */
 public class Main {
-
+	/**Método principal da função*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//Veiculo v1 =  new Veiculo();
-		CriaVeiculo v2 = new CriaVeiculo();
-		
+		//CriaVeiculo v2 = new CriaVeiculo();
+		/**Declarando um objeto usuário*/
 		Usuario u2 = new Usuario(JOptionPane.showInputDialog("Por favor, digite seu nome: "), 
 				Integer.parseInt(JOptionPane.showInputDialog("Por favor, digite sua idade: ")));
 		
+		/**Fazendo testes dos métodos*/
 		u2.ligarCarro();
 		u2.virarDireita();
 		u2.ligaAr();
