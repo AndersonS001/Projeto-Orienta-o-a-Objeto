607bd1e1e3bb6c9bce849ab79bb38f0e9a14f3c5
Anderson Aguiar de Souza <31575796+AndersonS001@users.noreply.github.com>
2017-09-30 08:55:42 -0300

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -10,7 +10,10 @@ public class Usuario extends CriaVeiculo{
 	private int Idade;
 	private boolean User;
 	 
-	/**Construtor da classe*/
+	/**Construtor da classe
+	 * @param String nome
+	 * @param int idade
+	 */
 	public Usuario(String nome, int idade){
 		/**Condicional de criação*/
 		if(nome == ""){
@@ -27,38 +30,61 @@ public class Usuario extends CriaVeiculo{
 		}
 	}
 	
-	 /**Método set para Nome*/
+	 /**Método set para Nome
+	  * @param String nome - Nome do usuário
+	  */
 	private void setNome(String nome){
 		this.Nome =  nome; /** Alterando o atributo*/
 	}
 	
-	/**Método set para Idade*/
+	/**Método set para Idade
+	 * @param  int idade - Idade do usuário
+     */
 	private void setIdade(int idade){
 		if(idade >= 18)
 			this.Idade =  idade; /** Alterando o atributo*/
 		else
 			JOptionPane.showMessageDialog(null, 
 					"Desculpe, mas você não possui 18 anos para poder dirigir", 
-					null, JOptionPane.ERROR_MESSAGE); /**Retorno erro*/
+					null, JOptionPane.ERROR_MESSAGE);
 	}
 	
-	/**Método set para Usuário*/
+	/**Método set para Usuário
+     * @param  boolean - Nome para o usuário
+     */
 	private void setUser(boolean x) {
-		this.User = x; /** Alterando o atributo*/
+		this.User = x;
 	}
 	
-	/**Método get para o Nome*/
+	/**Método get para o Nome
+	 * @return String - Retorna o nome do usuário.
+     */
 	public String getNome(){
-		return this.Nome;  /**@return Retornando o atributo*/
+		return this.Nome;  /**@return Nome*/
 	}
 
-	/**Método get para Idade*/
+	/**Método get para Idade
+	 * @return int - Retorna a idade do usuário.
+     */
 	public int getIdade(){
-		return this.Idade; /**@return Retornando o atributo*/
+		return this.Idade; /**@return Idade*/
 	}
 	
-	/**Método get para o Usuario*/
+	/**Método get para o Usuario
+	 * @return boolean - Retorna positivo se usuário existe, negativo se não.
+     */
 	public boolean getUser() {
-		return User; /**@return Retornando o atributo*/
+		return User; 
 	}
 }
+
+
+
+/**Método para calculo do valor da bonificação baseada na
+ * seguinte faixa de valores: Para vendas menores de
+ * 25.000,00, o percentual de comissão aplicado será de 5%, e * para valores iguais ou maiores de 25.000,00, o percentual
+ * será de 10%
+ * @author Manoel Pimentel
+ * @param  valorVendas - Valor total das vendas do mês
+ * @return Double - Valor do resultado do cálculo conforme a faixa de comissões.
+ */
