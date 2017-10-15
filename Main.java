107c5e159f786abc48efee5edc5da5219a46a32d
Anderson Aguiar de Souza <31575796+AndersonS001@users.noreply.github.com>
2017-10-15 21:12:00 -0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,31 +1,82 @@
-import javax.swing.JOptionPane;
-/**
- * 
- * @author Anderson Aguiar de Souza 22216001-2
- *Classe principal da função
- */
-public class Main {
-	/**Método principal da função*/
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//Veiculo v1 =  new Veiculo();
-		//CriaVeiculo v2 = new CriaVeiculo();
-		/**Declarando um objeto usuário*/
-		Usuario u2 = new Usuario(JOptionPane.showInputDialog("Por favor, digite seu nome: "), 
-				JOptionPane.showConfirmDialog(null,"Você possui mais de 18 anos? ",null, JOptionPane.YES_NO_OPTION));
+import java.awt.EventQueue;
 
-		if(u2.getUser()) {
-			/**Fazendo testes dos métodos*/
-			u2.ligarCarro();
-			u2.virarDireita();
-			u2.ligaAr();
-			u2.ligarCarro();
-			u2.ligaAr();
-			u2.fecharJanela();
-			u2.ligaAr();
-			u2.travaPorta();
-			u2.desligarCarro();
-		}
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JMenu;
+
+public class Main {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 434, 19);
+		frame.getContentPane().add(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Usu\u00E1rio");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Novo Usu\u00E1rio");
+		mntmNewMenuItem.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Usuario u = new Usuario();
+				u.Tela();
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Login");
+		mntmNewMenuItem_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				Login frame = new Login();
+				frame.Tela();
+			}
+		});
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		JMenu mnNewMenu_2 = new JMenu("Sair");
+		mnNewMenu_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				System.exit(0);
+			}
+		});
+		menuBar.add(mnNewMenu_2);
 	}
 }
