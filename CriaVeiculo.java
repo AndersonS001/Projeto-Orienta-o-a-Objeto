c271be0967947538ed84cb6c3b4a4bbe887f56bd
Anderson Aguiar de Souza <31575796+AndersonS001@users.noreply.github.com>
2017-11-18 22:31:56 -0200

    Update CriaVeiculo.java

--- CriaVeiculo.java
+++ CriaVeiculo.java
@@ -1,152 +1,113 @@
+package classes;
 import javax.swing.*;
 /**
  * 
  * @author Anderson Aguiar de Souza 22216001-2
- * A classe Cria Veiculo, herda os dados da classe veículo e adiciona propriedades mais específicas.
+ * A classe Cria Veiculo, herda os dados da classe veÃ­culo e adiciona propriedades mais especÃ­ficas.
  */
 public class CriaVeiculo extends Veiculo {
 	/**
 	 * Atributos privados da classe.
 	 */
-	private boolean ArCondicionado = false;
-	private boolean JanelaAberta = false;
-	private boolean PortaDestravada = false;
-	
+	private boolean ArCondicionado;
+	private String estacaoRadio;
 	
 	/**
 	 * Construtor da classe.
+	 * @param ar
+	 * @param janela
+	 * @param porta
+	 * @param estacao
 	 */
-	public CriaVeiculo(){//boolean Janela){
-		//JanelaAberta = Janela;
+	public CriaVeiculo(boolean ar, boolean janela, boolean porta, String estacao){
+		setArCondicionado(ar);
+		setJanela(janela);
+		setPorta(porta);
+		setEstacaoRadio(estacao);
 	}
 	
 	/**
-	 * Método público para ligar o Ar condicionado
+	 * MÃ©todo pÃºblico para ligar ou desligar o Ar condicionado
 	 */
-	public void ligaAr(){
-		if(!getArCondicionado() && !getJanelaAberta()){ /** Condicional para ligação do ar*/
-			//System.out.println("Ar condicionado ligado");
-			JOptionPane.showMessageDialog(null, "Ar condicionado ligado"); /** Mensagem de sucesso na condição*/
-			setArCondicionado(true); /** Chamando método set que seta o ar em ligado.*/
+	public void ligdesAr(){
+		if(!isArCondicionado() && !isJanela()){ 							/* Condicional para ligaï¿½ï¿½o do ar*/
+			JOptionPane.showMessageDialog(null, "Ligando ar condicionado"); /* Mensagem de sucesso na condiÃ§Ã£o*/
+			setArCondicionado(true); 										/* Chamando MÃ©todo set que seta o ar em ligado.*/
 		}
-		/***
-		 * Se não
-		 */
-		else
-			//System.out.println("Por favor, feche a janela antes de ligar o ar");
-			JOptionPane.showMessageDialog(null, "Por favor, feche a janela antes de ligar o ar"); /** Mensagem de erro na condição*/
-	}
-	/**
-	 * Método público para desligar o Ar Condicionado.
-	 */
-	public void desligaAr(){
-		/** Condicional para garantir que o Ar esteja ligado.*/
-		if(getArCondicionado()){ 
-			//System.out.println("Ar desligado");
-			/**Mensagem de sucesso na condição*/
-			JOptionPane.showMessageDialog(null, "Ar desligado");
-			/**Método set que altera o atribur ArCondicionado*/
+		else if(!isArCondicionado() && isJanela()) {  //Fecha a Janela e liga o Ar condicionado
+			fecharJanela();
+			JOptionPane.showMessageDialog(null, "Ligando ar condicionado");
+			setArCondicionado(true);
+		}
+		else { // Desliga o ar
+			JOptionPane.showMessageDialog(null, "Desligando ar condicionado");
 			setArCondicionado(false);
 		}
-		/**Se não*/
-		else
-			//System.out.println("O Ar ja está desligado");
-			/**Mensagem de erro na condição*/
-			JOptionPane.showMessageDialog(null, "O Ar ja está desligado");
 	}
-	/**
-	 * Método público que trava a porta do carro.
-	 */
+
 	public void travaPorta(){
-		/**Condicional para travar a porta*/
-		if(getCarroLigado() && getCarroAndando()){
-			/**Mensagem de sucesso na condição*/
-			JOptionPane.showMessageDialog(null, "Porta travada");
-			/**Setando o atributo Porta Destravada em falso*/
-			setPortaDestravada(false);
-		}else{
-			/**Mensagem de erro na condição*/
-			JOptionPane.showMessageDialog(null, "Seu carro não está em movimento, não se preocupe, em breve será travado");
-			//System.out.println("Seu carro não está em movimento, não se preocupe, em breve será travado");
-		}	
-	}
-	/**
-	 * Método público que destrava a porta do carro.
-	 */
-	public void destravaPorta() {
-		/**Condicional para destravar a porta*/
-		if(getCarroLigado() && !getCarroAndando()) {
-			/**Mensagem de sucesso na condição*/
-			JOptionPane.showMessageDialog(null, "Porta destravada");
-			/**Setando o atributo Porta Destravada em verdadeiro*/
-			setPortaDestravada(true);
-		}else{
-			/**Mensagem de erro na condição*/
-			JOptionPane.showMessageDialog(null, "A porta do seu carro já está destravada");
-			//System.out.println("Seu carro não está em movimento, não se preocupe, em breve será travado");
-		}	
+		JOptionPane.showMessageDialog(null, "Porta travada");
+		setPorta(true);
 	}
 	
-	/**
-	 * Método público que abre a janela do carro.
-	 */
-	public void abrirJanela() {
-		/**Condicional para abrir a janela*/
-		if(!getJanelaAberta()) {
-			/** Setando o atributo como verdadeiro*/
-			setJanelaAberta(true);
-			//System.out.println("Abrindo a janela");
-			/**Mensagem de sucesso na condição*/
+	/** MÃ©todo pÃºblico que destrava a porta do carro. */
+	public void destravaPorta() {										/*Condicional para destravar a porta*/
+			JOptionPane.showMessageDialog(null, "Porta destravada");
+			setPorta(false);
+	}
+	
+	/**MÃ©todo pÃºblico que abre a janela do carro.*/
+	public void abrirJanela() { /*Condicional para abrir a janela*/
+		if(!isJanela() && isArCondicionado()) {
+			setJanela(true);
 			JOptionPane.showMessageDialog(null, "Abrindo a janela");
-		}else {
-			//System.out.println("Janela ja aberta");
-			/**Mensagem de erro na condição*/
-			JOptionPane.showMessageDialog(null, "Janela já aberta");
+		}else if(!isJanela() && !isArCondicionado()) {
+			setJanela(true);
+			JOptionPane.showMessageDialog(null, "Abrindo a janela");
 		}
 	}
-	/**
-	 * Método público para fechar a janela
-	 */
-	public void fecharJanela() {
-		/** Condicional para fechar a janela*/
-		if(getJanelaAberta()) {
-			/**Setando o atributo da janela como aberta*/
-			setJanelaAberta(false);
-			//System.out.println("Fechando a janela
-			/**Mensagem de sucesso na condição*/
+	
+	
+	/** MÃ©todo pï¿½blico para fechar a janela */
+	public void fecharJanela() {	/* Condicional para fechar a janela*/
+		if(isJanela()) {			/*Setando o atributo da janela como aberta*/
+			setJanela(false);
 			JOptionPane.showMessageDialog(null, "Fechando a janela");
-		}else{
-			//System.out.println("Janela já fechada");
-			/**Mensagem de erro na condição*/
-			JOptionPane.showMessageDialog(null, "Janela já fechada");
 		}
 	}
-		
-	/**Método set privado do atributo PortaDestravada*/
-	private void setPortaDestravada(boolean cond) {
-		PortaDestravada = cond;
-	}
-	/**Método get protegido do atributo PortaDestravada*/
-	protected boolean getPortaDestravada() {
-		/**Retorno da função*/
-		return PortaDestravada; 
-	}
-	/**Método set privado do atributo Janela aberta*/
-	private void setJanelaAberta(boolean cond){
-		JanelaAberta = cond;
-	}
-	/**Método get protegido do atributo Janela aberta*/
-	protected boolean getJanelaAberta() {
-		/**Retorno da função*/
-		return JanelaAberta;
-	}
-	/**Método set privado do atributo ArCondicionado*/
+	
+	/**MÃ©todo set privado do atributo ArCondicionado*/
 	private void setArCondicionado(boolean cond){
 		ArCondicionado = cond;
 	}
-	/**Método get protegido do atributo ArCondicionado*/
-	protected boolean getArCondicionado() {
-		/**Retorno da função*/
+	
+	/**MÃ©todo get do atributo ArCondicionado*/
+	public boolean isArCondicionado() {
+		/**Retorno da funï¿½ï¿½o*/
 		return ArCondicionado;
+	}
+	
+	/**MÃ©todo get da estaÃ§Ã£o de RÃ¡dio*/
+	public String getEstacaoRadio() {
+		return estacaoRadio;
+	}
+	
+	/**MÃ©todo set da estaÃ§Ã£o de RÃ¡dio*/
+	public void setEstacaoRadio(String estacaoRadio) {
+		this.estacaoRadio = estacaoRadio;
+	}
+
+	@Override
+	public void ligarCarro() {
+		if(!getCarroLigado()){										/* Condicional para garantir que o carro nï¿½o esteja ligado*/
+			JOptionPane.showMessageDialog(null, "Ligando o carro"); /* Mensagem de sucesso na condiÃ§Ã£o*/
+			setCarroLigado(true); 									/* Passando a informaÃ§Ã£o para o MÃ©todo set do carroLigado*/
+		}
+		/** Se nï¿½o*/
+		else{
+			JOptionPane.showMessageDialog(null, "Desligando o carro"); /* Desliga o carro*/
+			setCarroLigado(false);
+		}
+		
 	}
 }
