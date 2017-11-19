158b177ab4fd830b110f1429735051103b9be2a0
Anderson Aguiar de Souza <31575796+AndersonS001@users.noreply.github.com>
2017-11-18 22:34:36 -0200

    Update LoginUsuario.java

--- LoginUsuario.java
+++ LoginUsuario.java
@@ -1,10 +1,16 @@
+package view;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controlers.Arquivo;
+import controlers.PainelDeControle;
+
 import javax.swing.JButton;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
 import javax.swing.JMenu;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
@@ -16,37 +22,41 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JPasswordField;
 
+/**
+ * Classe de Login do usuário.
+ * @author 
+ */
 public class LoginUsuario {
 
 	private JPanel contentPane;
 	
-	Usuario u = new Usuario();
 	ArrayList<String> v = new ArrayList<String>();
+	private JPasswordField senha;
 	
 	/**
-	 * Launch the application.
+	 * Inicia a aplicação
 	 */
 	public LoginUsuario() {
 		try {
-			v = Arquivo.getUser();
+			v = Arquivo.getUser(false);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-
 	/**
-	 * Create the frame.
 	 * @wbp.parser.entryPoint
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public void Tela() {
 		JFrame x = new JFrame();
+		x.setTitle("Login");
 		x.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		x.setVisible(true);
-		x.setBounds(100, 100, 353, 188);
+		x.setBounds(100, 100, 377, 245);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		x.setContentPane(contentPane);
@@ -66,29 +76,59 @@ public class LoginUsuario {
 		menuBar.add(mnNewMenu);
 		
 		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		comboBox.setModel(new DefaultComboBoxModel(v.toArray()));
-		comboBox.setBounds(20, 74, 189, 21);
+		comboBox.setBounds(118, 34, 208, 29);
 		contentPane.add(comboBox);
 		
 		JLabel lblQuemVoc = new JLabel("Quem \u00E9 voc\u00EA?");
-		lblQuemVoc.setFont(new Font("Arial", Font.ITALIC, 17));
-		lblQuemVoc.setBounds(20, 32, 277, 31);
+		lblQuemVoc.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblQuemVoc.setBounds(20, 32, 112, 31);
 		contentPane.add(lblQuemVoc);
 		
-		JButton btnEntrar = new JButton("Entrar");
 		
+		
+		JLabel lblSenha = new JLabel("Senha/CNH: ");
+		lblSenha.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblSenha.setBounds(20, 91, 100, 21);
+		contentPane.add(lblSenha);
+		
+		senha = new JPasswordField();
+		senha.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		senha.setBounds(118, 90, 208, 22);
+		contentPane.add(senha);
+		
+		JButton btnEntrar = new JButton("Entrar");
 		if(v.size() == 0) 
 			btnEntrar.setVisible(false);
+		btnEntrar.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		
 		btnEntrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//comboBox.getSelectedItem().toString();
-				PainelDeControle p =  new PainelDeControle();
-				p.setVisible(true);
-				x.dispose();
+				ArrayList<String> senhas = new ArrayList<String>();
+				
+				try {
+					senhas = Arquivo.getSenhas(false);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				/*Vari�vel respons�vel por pegar a senha digitada
+				 * pelo usu�rio
+				 */
+				String auxSenha = new String(senha.getPassword());
+				
+				if(auxSenha.equals(senhas.get(comboBox.getSelectedIndex()*2 )) || 
+						auxSenha.equals(senhas.get(comboBox.getSelectedIndex()*2+1) )) {
+					new PainelDeControle(comboBox.getSelectedItem().toString(), comboBox.getSelectedIndex());
+					x.dispose();
+				}
+				else 
+					JOptionPane.showMessageDialog(null,"Senha/CNH incorreta");
+				senha.setText(""); //limpar o campo da senha
 			}
 		});
-		btnEntrar.setBounds(20, 115, 89, 23);
+		btnEntrar.setBounds(20, 147, 101, 38);
 		contentPane.add(btnEntrar);		
 	}
 }
